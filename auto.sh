97d39e1627ddc1aab7093b025c4b593a3fcced05
N1TR0 <b.suvonov2022@gmail.com>
2023-11-03 13:27:37 +0800

    auto commit - 1698989257

---
+++ auto.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+project_dir="$HOME/auto-commit"
+
+ts=$(date +%s)
+echo "$ts" > $project_dir/main.txt
+git -C $project_dir add .
+git -C $project_dir commit -m "auto commit - $ts"
+git -C $project_dir push origin master
